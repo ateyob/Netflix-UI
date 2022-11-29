aeea6f9a56c83e819b8a6a57448892c1bc932522
Eyob Kefyalew <ateyob@gmail.com>
2022-11-29 18:01:57 +0300

    First

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import requests from "../Requests";
+
+const Main = () => {
+  const [movies, setmovies] = useState([]);
+
+  useEffect(() => {}, []);
+
+  return <div>Main</div>;
+};
+
+export default Main;
